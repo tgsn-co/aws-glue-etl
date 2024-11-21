e4012a04a13f8a019baaf22fe7c449d451d013d6
Andrea La Rosa <drewlr@users.noreply.github.com>
2024-11-21 14:33:38 +0000

    Synchronizing job KoBo to Bronze - Script from AWS Glue at 2024/11/21 14:33:38

--- KoBo to Bronze - Script/KoBo to Bronze - Script.py
+++ KoBo to Bronze - Script/KoBo to Bronze - Script.py
@@ -5,37 +5,49 @@ from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
-  
+
+from datetime import datetime
+import pandas as pd
+from pyspark.sql.functions import to_timestamp
+
 sc = SparkContext.getOrCreate()
 glueContext = GlueContext(sc)
 spark = glueContext.spark_session
 job = Job(glueContext)
-dyf = glueContext.create_dynamic_frame.from_catalog(database='database_name', table_name='table_name')
-dyf.printSchema()
-df = dyf.toDF()
-df.show()
-import matplotlib.pyplot as plt
+# Read CSV files from KoBo landing layer
+dyf_kobo_landing = glueContext.create_dynamic_frame.from_options(
+    format_options={"quoteChar": "\"", "withHeader": True, "separator": "|"}, 
+    connection_type="s3", format="csv", 
+    connection_options={"paths": ["s3://tgsn-landing/dummy_kobo_data_landing/"], "recurse": True}, 
+    transformation_ctx="Amaz|onS3")
 
-# Set X-axis and Y-axis values
-x = [5, 2, 8, 4, 9]
-y = [10, 4, 8, 5, 2]
-  
-# Create a bar chart 
-plt.bar(x, y)
-  
-# Show the plot
+dyf_kobo_landing.printSchema()
+# dyf_kobo_landing = glueContext.create_dynamic_frame.from_catalog(database='tgsn_landing', table_name='kobo_data_landing')
+# dyf_kobo_landing.printSchema()
+spark_df_kobo = dyf_kobo_landing.toDF()
+spark_df_kobo = spark_df_kobo.withColumn("_submission_time",to_timestamp("_submission_time"))
+spark_df_kobo_today = spark_df_kobo.where(spark_df_kobo['_submission_time'] >= datetime.today().strftime('%Y-%m-%d'))
+# spark_df_kobo.write.mode('overwrite') \
+#         .format('parquet') \
+#         .save('s3://tgsn-bronze/kobo/Moth/')
+from awsglue.dynamicframe import DynamicFrame
+
+# Convert Spark DataFrame to Glue DynamicFrame 
+df_kobo = DynamicFrame.fromDF(spark_df_kobo_today, glueContext, "df_kobo")
+
 s3output = glueContext.getSink(
-  path="s3://bucket_name/folder_name",
+  path="s3://tgsn-bronze/kobo/Moth/",
   connection_type="s3",
   updateBehavior="UPDATE_IN_DATABASE",
   partitionKeys=[],
   compression="snappy",
   enableUpdateCatalog=True,
-  transformation_ctx="s3output",
+  transformation_ctx="s3output"
 )
 s3output.setCatalogInfo(
-  catalogDatabase="demo", catalogTableName="populations"
+  catalogDatabase="tgsn_bronze", catalogTableName="kobo_moth"
 )
 s3output.setFormat("glueparquet")
-s3output.writeFrame(DyF)
+s3output.writeFrame(df_kobo)
+# 
 job.commit()
