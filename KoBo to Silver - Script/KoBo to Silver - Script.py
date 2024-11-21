6a158acc713d68ce60bc1d86bdc171feaae2210d
Andrea La Rosa <drewlr@users.noreply.github.com>
2024-11-21 11:40:35 +0000

    Synchronizing job KoBo to Silver - Script from AWS Glue at 2024/11/21 11:40:35

--- KoBo to Silver - Script/KoBo to Silver - Script.py
+++ KoBo to Silver - Script/KoBo to Silver - Script.py
@@ -5,7 +5,11 @@ from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
-  
+
+from datetime import datetime
+import pandas as pd
+from pyspark.sql.functions import to_timestamp
+
 sc = SparkContext.getOrCreate()
 glueContext = GlueContext(sc)
 spark = glueContext.spark_session
@@ -48,28 +52,27 @@ cast(Hight as double) as Hight,
 cast(How_many_family_members_live_with_you as int) as No_Family_Members,
 to_date(_submission_time) as Submission_Time
 FROM kobo_bronze
-WHERE to_date(_submission_time) >= current_date()
 order by _id
 '''
 spark_df_kobo_silver = spark.sql(query_to_silver)
 spark_df_kobo_silver.show()
-from awsglue.dynamicframe import DynamicFrame
-
-# Convert Spark DataFrame to Glue DynamicFrame 
-df_kobo = DynamicFrame.fromDF(spark_df_kobo_silver, glueContext, "df_kobo")
-
-s3output = glueContext.getSink(
-  path="s3://tgsn-silver-bucket/kobo/Moth/",
-  connection_type="s3",
-  updateBehavior="UPDATE_IN_DATABASE",
-  partitionKeys=[],
-  compression="snappy",
-  enableUpdateCatalog=True,
-  transformation_ctx="s3output"
-)
-s3output.setCatalogInfo(
-  catalogDatabase="tgsn_silver", catalogTableName="kobo_moth"
-)
-s3output.setFormat("glueparquet")
-s3output.writeFrame(df_kobo)
+# Script generated for node Amazon S3
+additional_options = {}
+tables_collection = spark.catalog.listTables("tgsn_silver")
+table_names_in_db = [table.name for table in tables_collection]
+table_exists = "kobo_moth" in table_names_in_db
+if table_exists:
+    spark_df_kobo_silver.writeTo("glue_catalog.tgsn_silver.kobo_moth") \
+        .tableProperty("format-version", "2") \
+        .tableProperty("location", "s3://tgsn-silver-bucket/kobo/Moth/meta/tgsn_silver/kobo_moth") \
+        .tableProperty("write.parquet.compression-codec", "gzip") \
+        .options(**additional_options) \
+.append()
+else:
+    spark_df_kobo_silver.writeTo("glue_catalog.tgsn_silver.kobo_moth") \
+        .tableProperty("format-version", "2") \
+        .tableProperty("location", "s3://tgsn-silver-bucket/kobo/Moth/meta/tgsn_silver/kobo_moth") \
+        .tableProperty("write.parquet.compression-codec", "gzip") \
+        .options(**additional_options) \
+.create()
 job.commit()
